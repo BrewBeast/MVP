e935523f55a6b1d540aa3eb984363f5457d71dda
Matthias Fore <matthiasfore@gmail.com>
2019-11-16 14:52:05 -0800

    added api key

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static(path.join(__dirname, '.')));
+app.use('/', express.static(path.join(__dirname, './public')));
 
 app.listen(port, () => console.log('live on : ', port));
---
+++ server/key.js
@@ -0,0 +1,8 @@
+
+const headers = headers({
+  'x-rapidapi-host': 'community-hacker-news-v1.p.rapidapi.com',
+  'x-rapidapi-key': '9eb4c6e766mshbbf02cfb5acc9dcp103e52jsn1beadbf88207',
+});
+
+module.exports = headers
+;
