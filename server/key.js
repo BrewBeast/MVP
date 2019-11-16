dbbb9e1b7f380dcebe2fd8a3a2c523fc140ff66a
Matthias Fore <matthiasfore@gmail.com>
2019-11-16 14:56:18 -0800

    Update key.js

--- server/key.js
+++ server/key.js
@@ -1,7 +1,7 @@
 
 const headers = headers({
   'x-rapidapi-host': 'community-hacker-news-v1.p.rapidapi.com',
-  'x-rapidapi-key': '9eb4c6e766mshbbf02cfb5acc9dcp103e52jsn1beadbf88207',
+  'x-rapidapi-key': '',
 });
 
 module.exports = headers
